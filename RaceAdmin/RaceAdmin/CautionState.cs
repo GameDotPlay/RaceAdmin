b422eaecdbf0ea3605792393fa9c62a9a81dc9cd
Game.Play() <gamedotplay0@gmail.com>
2021-11-26 14:17:04 -0500

    Created
    Moved CautionState enum to its own file.

---
+++ RaceAdmin/RaceAdmin/CautionState.cs
@@ -0,0 +1,25 @@
+﻿namespace RaceAdmin
+{
+    /// <summary>
+    /// Tracks the current state of the caution handling system.
+    /// </summary>
+	public enum CautionState
+	{
+        /// <summary>
+        /// Initial state.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Indicates that the application incident threshold for a caution 
+        /// has been reached.
+        /// </summary>
+        ThresholdReached,
+
+        /// <summary>
+        /// Indicates that a full course yellow has been initiated within
+        /// iRacing.
+        /// </summary>
+        YellowFlagDeployed
+    }
+}
