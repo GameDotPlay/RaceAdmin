59d9dbc2fc97dd51814744f6b5223b3246406ea2
Game.Play() <gamedotplay0@gmail.com>
2021-11-26 16:02:25 -0500

    Added
    Added new incident class.
    Will be used to log incidents and the timestamp will be used to filter incidents temporily.

---
+++ RaceAdmin/RaceAdmin/Incident.cs
@@ -0,0 +1,19 @@
+﻿namespace RaceAdmin
+{
+	using System;
+	public class Incident
+	{
+		public Incident(double updateTime, DateTime timeStamp, int value, int carIdx)
+		{
+			UpdateTime = updateTime;
+			TimeStamp = timeStamp;
+			Value = value;
+			CarIdx = carIdx;
+		}
+
+		public double UpdateTime { get; set; }
+		public DateTime TimeStamp { get; set; }
+		public int Value { get; set; }
+		public int CarIdx { get; set; }
+	}
+}
