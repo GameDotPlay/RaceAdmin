4fb454d3ee100ad35e1a733c343c18d352ead4c4
CaptJeanLuc1701 <erich.smith@att.net>
2021-12-03 09:06:16 -0500

    Add
    Added enum for incident table column widths.

---
+++ RaceAdmin/RaceAdmin/IncidentTableColumnWidths.cs
@@ -0,0 +1,15 @@
+﻿namespace RaceAdmin
+{
+	public enum IncidentTableColumnWidths
+	{
+		LocalTimeColumnWidth = 117,
+		SessionTimeColumnWidth = 137,
+		CarClassColumnWidth = 115,
+		CarNumberColumnWidth = 74,
+		TeamNameColumnWidth = 200,
+		DriverNameColumnWidth = 200,
+		IncidentValueColumnWidth = 95,
+		TotalIncidentsColumnWidth = 95,
+		DriverLapNumberColumnWidth = 78
+	}
+}
