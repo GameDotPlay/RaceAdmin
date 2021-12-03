06515607f636ed1839127cce0eadc41d50638a64
CaptJeanLuc1701 <erich.smith@att.net>
2021-12-03 17:09:18 -0500

    Added
    Added color picker icon.

--- RaceAdmin/RaceAdmin/Properties/Resources.Designer.cs
+++ RaceAdmin/RaceAdmin/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace RaceAdmin.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ColorPickerIcon {
+            get {
+                object obj = ResourceManager.GetObject("ColorPickerIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ConeEmpty {
             get {
                 object obj = ResourceManager.GetObject("ConeEmpty", resourceCulture);
