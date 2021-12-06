62b5aeef364f0cfc76ba7d5a7a58979df747dacd
Game.Play() <gamedotplay0@gmail.com>
2021-12-06 07:41:04 -0500

    Add
    Added DebugTableColumnWidths enum.

---
+++ RaceAdmin/RaceAdmin/DebugTableColumnWidths.cs
@@ -0,0 +1,18 @@
+﻿namespace RaceAdmin
+{
+	public enum DebugTableColumnWidths
+	{
+		CarIdxColumnWidth = 83,
+		CarNumberColumnWidth = 74,
+		CarOverallPositionColumnWidth = 100,
+		CarClassPositionColumnWidth = 144,
+		CarClassShortNameColumnWidth = 125,
+		DriverNameColumnWidth = 200,
+		CarPercentAroundTrackColumnWidth = 175,
+		CarBetweenPitConesColumnWidth = 110,
+		CarCurrentLapColumnWidth = 125,
+		CarLapsCompletedColumnWidth = 160,
+		CarTrackSurfaceColumnWidth = 150,
+		CarTrackSurfaceMaterialColumnWidth = 150
+	};
+}
